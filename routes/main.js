d608a722bd4034c47f469b020c8fc8e5e67b9a63
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-21 23:36:34 +0300

    update games.js

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,10 @@
 const mainRoute = require("express").Router();
+const fs = require("fs").promises;
 
 mainRoute.get("/", (req, res) => {
-  res.status(200).send("<h1>The interface will be here soon</h1>");
+  fs.readFile("./public/index.html", "utf8").then((data) => {
+    res.header("Content-Type", "text/html").send(data);
+  });
 });
 
 module.exports = mainRoute;
