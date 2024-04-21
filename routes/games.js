ad39e65e28c5ccd82db95f905fc055e62510dd94
1MishGun1 <mikhail.kudrov@gmail.com>
2024-04-21 23:36:30 +0300

    update games.js

--- routes/games.js
+++ routes/games.js
@@ -1,57 +1,13 @@
 const gamesRouter = require("express").Router();
-const { readData, writeData } = require("../utils/data");
+const {
+  sendAllGames,
+  deleteGame,
+  addGameController,
+} = require("../controllers/games");
+const { getAllGames } = require("../middlewares/games");
 
-//? Getting games
-const getAllGames = async (req, res) => {
-  const games = await readData("../utils/data/games.json");
-  if (!games) {
-    res.status(400);
-    res.end({
-      status: "error",
-      message: "No games in database. Please, you add new game",
-    });
-    return;
-  }
-  req.games = games;
-  res.send(req.games);
-};
-
-//? Delete game
-const deleteGame = async (req, res) => {
-  const games = await readData("../utils/data/games.json");
-  if (!games) {
-    res.status(400);
-    res.end({
-      status: "error",
-      message: "No games in database. Please, you add new game",
-    });
-    return;
-  }
-  req.games = games;
-
-  //? Read game by id
-  const id = Number(req.params.id);
-
-  //? Search game by id, which you need delete
-  res.game = req.games.find((item) => item.id === id);
-
-  //? Search game by id
-  const index = req.games.findIndex((item) => item.id === req.game.id);
-
-  //? Delete game
-  res.games.splice(index, 1);
-
-  //? Write update array in games.json
-  await writeData("../utils/data/games.json", req.games);
-
-  //? Show response about the operation performed
-  res.send({
-    games: req.games,
-    updated: req.game,
-  });
-
-  gamesRouter.get("/games", getAllGames);
-  gamesRouter.get("/games", deleteGame);
-};
+gamesRouter.post("/games", getAllGames, addGameController);
+gamesRouter.get("/games", getAllGames, sendAllGames);
+gamesRouter.delete("/games/:id", getAllGames, deleteGame);
 
 module.exports = gamesRouter;
